b6fe66feafb153f1edd6a892a54178a31f04d2a8
Matei Cananau <mateicananau@gmail.com>
2022-06-28 23:22:26 +0200

    Started Implementing Combat

--- main.py
+++ main.py
@@ -5,98 +5,100 @@ from os import path
 from settings import *
 from sprites import *
 
+
 class Game:
-	def __init__(self):
-		pg.init()
-		self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-		pg.display.set_caption(TITLE)
-		self.clock = pg.time.Clock()
-		pg.key.set_repeat(500, 100)
-		self.load_data()
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
 
-	def load_data(self):
-		game_folder = path.dirname(__file__)
-		self.map_data = []
-		with open(path.join(game_folder, 'map.txt'), 'rt') as f:
-			for line in f:
-				self.map_data.append(line)
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
 
-	def new(self):
-		self.all_sprites = pg.sprite.Group()
-		self.walls = pg.sprite.Group()
-		self.enemies = pg.sprite.Group()
-		for row, tiles in enumerate(self.map_data):
-			for col, tile in enumerate(tiles):
-				if tile == '1':
-					Wall(self, col, row)
-				if tile == 'p':
-					self.player = Player(self, col, row)
-					# self.player = Player(self, math.floor(G_WIDTH/2), G_HEIGHT-3)
-				if tile == 'e':
-					self.enemy = Enemy(self, col, row)
-					
-	
-	def run(self):
-		self.playing = True
-		while self.playing:
-			self.dt = self.clock.tick(FPS)/1000
-			self.events()
-			self.update()
-			self.draw()
-	
-	def quit(self):
-		pg.quit()
-		sys.exit()
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'p':
+                    self.player = Player(self, col, row)
+                    # self.player = Player(self, math.floor(G_WIDTH/2), G_HEIGHT-3)
+                if tile == 'e':
+                    self.enemy = Enemy(self, col, row)
 
-	def update(self):
-		self.all_sprites.update()
-	
-	def draw_grid(self):
-		for x in range(0, WIDTH, TILE_SIZE):
-			pg.draw.line(self.screen, (150, 150, 150), (x, 0), (x, HEIGHT))
-		for y in range(0, WIDTH, TILE_SIZE):
-			pg.draw.line(self.screen, (150, 150, 150), (0, y), (WIDTH, y))
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS)/1000
+            self.events()
+            self.update()
+            self.draw()
 
-	def draw(self):
-		self.screen.fill(BG_COLOR)
-		self.draw_grid()
-		self.all_sprites.draw(self.screen)
-		pg.display.flip()
-	
-	def events(self):
-		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				self.quit()
-			if event.type == pg.KEYDOWN:
-				if event.key == pg.K_ESCAPE:
-					self.quit()
-				if event.key == pg.K_LEFT:
-					self.player.move(dx=-1)
-				if event.key == pg.K_RIGHT:
-					self.player.move(dx=1)
-				if event.key == pg.K_UP:
-					self.player.move(dy=-1)
-				if event.key == pg.K_DOWN:
-					self.player.move(dy=1)
-				rand = random.randint(1, 4)
-				if rand == 1:
-					self.enemy.move(dx=-1)
-				if rand == 2:
-					self.enemy.move(dx=1)
-				if rand == 3:
-					self.enemy.move(dy=-1)
-				if rand == 4:
-					self.enemy.move(dy=1)
-	
-	def show_start_screen(self):
-		pass
+    def quit(self):
+        pg.quit()
+        sys.exit()
 
-	def show_go_screen(self):
-		pass
+    def update(self):
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILE_SIZE):
+            pg.draw.line(self.screen, (150, 150, 150), (x, 0), (x, HEIGHT))
+        for y in range(0, WIDTH, TILE_SIZE):
+            pg.draw.line(self.screen, (150, 150, 150), (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BG_COLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+                if not self.enemy.fighting:
+                    rand = random.randint(1, 4)
+                    if rand == 1:
+                        self.enemy.move(dx=-1)
+                    if rand == 2:
+                        self.enemy.move(dx=1)
+                    if rand == 3:
+                        self.enemy.move(dy=-1)
+                    if rand == 4:
+                        self.enemy.move(dy=1)
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
 
 g = Game()
 g.show_start_screen()
 while True:
-	g.new()
-	g.run()
-	g.show_go_screen()
+    g.new()
+    g.run()
+    g.show_go_screen()
--- sprites.py
+++ sprites.py
@@ -1,76 +1,83 @@
 import pygame as pg
 from settings import *
 
+
 class Player(pg.sprite.Sprite):
-	def __init__(self, game, x, y):
-		self.groups = game.all_sprites
-		pg.sprite.Sprite.__init__(self, self.groups)
-		self.game = game
-		self.image = pg.Surface((TILE_SIZE, TILE_SIZE))
-		self.image.fill((255, 255, 0))
-		self.rect = self.image.get_rect()
-		self.x = x
-		self.y = y
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILE_SIZE, TILE_SIZE))
+        self.image.fill((255, 255, 0))
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
 
-	def move(self, dx=0, dy=0):
-		if not self.collide_walls(dx, dy):
-			self.x += dx
-			self.y += dy
-	
-	def collide_walls(self, dx=0, dy=0):
-		for wall in self.game.walls:
-			if wall.x == self.x + dx and wall.y == self.y + dy:
-				return True
-		# for enemy in self.game.enemies:
-		# 	if enemy.x == self.x + dx and enemy.y == self.y + dy:
-		# 		return True
-		return False
+    def move(self, dx=0, dy=0):
+        if not self.collide_walls(dx, dy):
+            self.x += dx
+            self.y += dy
 
-	def update(self):
-		# 1 px => 64 px
-		self.rect.x = self.x * TILE_SIZE
-		self.rect.y = self.y * TILE_SIZE
+    def collide_walls(self, dx=0, dy=0):
+        for wall in self.game.walls:
+            if wall.x == self.x + dx and wall.y == self.y + dy:
+                return True
+        # for enemy in self.game.enemies:
+        # 	if enemy.x == self.x + dx and enemy.y == self.y + dy:
+        # 		return True
+        return False
+
+    def update(self):
+        # 1 px => 64 px
+        self.rect.x = self.x * TILE_SIZE
+        self.rect.y = self.y * TILE_SIZE
+
 
 class Enemy(pg.sprite.Sprite):
-	def __init__(self, game, x, y):
-		self.groups = game.all_sprites, game.walls
-		self.game = game
-		pg.sprite.Sprite.__init__(self, self.groups)
-		self.image = pg.Surface((TILE_SIZE, TILE_SIZE))
-		self.image.fill((255, 0, 0))
-		self.rect = self.image.get_rect()
-		self.x = x
-		self.y = y
-		self.rect.x = x * TILE_SIZE
-		self.rect.y = y * TILE_SIZE
-	
-	def move(self, dx=0, dy=0):
-		if not self.collide_walls(dx, dy):
-			self.x += dx
-			self.y += dy
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        self.game = game
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.image = pg.Surface((TILE_SIZE, TILE_SIZE))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILE_SIZE
+        self.rect.y = y * TILE_SIZE
+        self.health = 3
+        self.fighting = False
 
-	def collide_walls(self, dx=0, dy=0):
-		for wall in self.game.walls:
-			if wall.x == self.x + dx and wall.y == self.y + dy:
-				return True
-		if self.game.player.x == self.x + dx and self.game.player.y == self.y + dy:
-			return True
-		return False
+    def move(self, dx=0, dy=0):
+        if not self.collide_walls(dx, dy) and not self.fighting:
+            self.x += dx
+            self.y += dy
 
-	def update(self):
-		# 1 px => 64 px
-		self.rect.x = self.x * TILE_SIZE
-		self.rect.y = self.y * TILE_SIZE
+    def collide_walls(self, dx=0, dy=0):
+        for wall in self.game.walls:
+            if wall.x == self.x + dx and wall.y == self.y + dy:
+                return True
+        if self.game.player.x == self.x + dx and self.game.player.y == self.y + dy:
+            self.fighting = True
+        # self.fighting = False
+            return True
+        return False
+
+    def update(self):
+        # 1 px => 64 px
+        self.rect.x = self.x * TILE_SIZE
+        self.rect.y = self.y * TILE_SIZE
+
 
 class Wall(pg.sprite.Sprite):
-	def __init__(self, game, x, y):
-		self.groups = game.all_sprites, game.walls
-		pg.sprite.Sprite.__init__(self, self.groups)
-		self.game = game
-		self.image = pg.Surface((TILE_SIZE, TILE_SIZE))
-		self.image.fill((0, 255, 0))
-		self.rect = self.image.get_rect()
-		self.x = x
-		self.y = y
-		self.rect.x = x * TILE_SIZE
-		self.rect.y = y * TILE_SIZE
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILE_SIZE, TILE_SIZE))
+        self.image.fill((0, 255, 0))
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILE_SIZE
+        self.rect.y = y * TILE_SIZE
